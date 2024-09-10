0e50d5f31ebdd970edebf9724871a691ae968528
ejarao <irishegie.arao@ssu.edu.ph>
2024-09-10 21:00:25 +0800

    update: re-organized the codebase to OOP notation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -8,50 +9,12 @@ screen.bgcolor("black")
 screen.title("Py Snake Game")
 screen.tracer(0)
 
-def up(snk):
-    snk.setheading(90)
-    print("Snake goes up")  
-def down(snk):
-    snk.setheading(270)
-    print("Snake goes down")  
-def left (snk):
-    snk.setheading(180)
-    print("Snake goes left")  
-def right(snk):
-    snk.setheading(0)
-    print("Snake goes right ")  
+snake = Snake()
 
-snake = []
-starting_pos = [(0,0), (-20,0), (-40,0)]
-def starting_body():
-    for pos in starting_pos:
-        segment = Turtle()
-        segment.shape("square")
-        segment.color("white")
-        segment.penup()
-        segment.goto(pos)
-        snake.append(segment)
-
-starting_body()
-screen.listen()
-key_list = {
-    "w": up,
-    "a": left,
-    "s": down,
-    "d": right
-} 
 game_is_on = True
-for key, func in key_list.items():
-    screen.onkey(lambda f=func: f(snake[0]), key)
-
 while game_is_on:
     screen.update()
-    for segment in range(len(snake)-1,0,-1):
-        new_x = snake[segment-1].xcor()
-        new_y = snake[segment-1].ycor()
-        snake[segment].goto(new_x,new_y)
-        time.sleep(.1)
-
-    snake[0].fd(20)
+    snake.move()
+    time.sleep(0.075)
 
 screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,50 @@
+from turtle import Turtle, Screen
+screen = Screen()
+class Snake:
+    def __init__(self):
+        self.snake = []
+        self.starting_pos =  [(0,0), (-20,0), (-40,0)]
+        self.starting_body()      
+        self.key_list = {
+        "w": self.up,
+        "a": self.left,
+        "s": self.down,
+        "d": self.right    }
+        self.direction()
+    def up(self):
+        self.snake[0].setheading(90)
+        print("Snake goes up")  
+    def down(self):
+        self.snake[0].setheading(270)
+        print("Snake goes down")  
+    def left (self):
+        self.snake[0].setheading(180)
+        print("Snake goes left")  
+    def right(self):
+        self.snake[0].setheading(0)
+        print("Snake goes right ")  
+    def starting_body(self):
+        """Creating the initial Snake Body from 3 squares appended in the "snake" list"""
+        for pos in self.starting_pos:
+            segment = Turtle()
+            segment.shape("square")
+            segment.color("white")
+            segment.penup()
+            segment.goto(pos)
+            self.snake.append(segment)
+    def direction(self):
+        """Defines the direction of the snake motion"""
+        for key, func in self.key_list.items():
+            screen.onkey(func, key)  
+        screen.listen()  # Listen for the keypress events
+    def move(self):
+        """Moves the snake forward by updating each segment's position.
+        Each segment follows the segment in front of it, starting from the tail 
+        to the second segment, while the head moves forward by 20 units.
+        """
+        for segment in range(len(self.snake)-1,0,-1):
+            new_x = self.snake[segment-1].xcor()
+            new_y = self.snake[segment-1].ycor()
+            self.snake[segment].goto(new_x,new_y)
+        self.snake[0].fd(20)
+
